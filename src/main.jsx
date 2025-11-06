f0bf09d85ccf2bfdc11bffab8bf60868491316eb
daniahaitham <dania.haitamm@gmail.com>
2025-11-07 01:58:38 +0300

    Fix Vercel routing using HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { HashRouter } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <HashRouter>
+  <App />
+</HashRouter>
 )
