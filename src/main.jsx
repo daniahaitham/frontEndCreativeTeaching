e68edd9abb41b22594908e7d79298d753c635e30
daniahaitham <dania.haitamm@gmail.com>
2025-11-07 09:03:46 +0300

    final vercel hashrouter fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+ import App from './App'
 import './index.css'
 import { HashRouter } from "react-router-dom";
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <HashRouter>
-  <App />
-</HashRouter>
-)
+    <App />
+  </HashRouter>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: "dist",
   },
-  server: { port: 5173 },
-  // 👇 this line forces all client routes to resolve correctly
-  base: '/'
-})
+});
